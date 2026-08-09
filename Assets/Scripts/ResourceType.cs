5847a650e64241ef7f2d303320a4564314d0c206
Malte95 <maltescherenberg@gmx.de>
2026-08-09 18:29:09 +0200

    Add ResourceType ScriptableObject and Wood/Stone/Grain assets

---
+++ Assets/Scripts/ResourceType.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewResourceType", menuName = "Siedler/ResourceType")]
+public class ResourceType : ScriptableObject
+{
+    public string resourceName;
+    public string description;
+    
+}
